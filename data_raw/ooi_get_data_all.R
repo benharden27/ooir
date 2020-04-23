97c37a7911bc7f83aeade7d2fb75fc579b4c5462
benharden27 <benharden27@gmail.com>
2020-04-23 12:31:49 -0400

    add data reading script to act as a template for sending request and aid in development of helper functions

---
+++ data_raw/ooi_get_data_all.R
@@ -0,0 +1,172 @@
+library(ooir)
+library(tidync)
+library(tidyverse)
+
+# Set up parameters -------------------------------------------------------
+
+# which field do you want to download?
+field <- "ctd" # "do"
+csv_write <- TRUE # write the csv?
+csv_file <- "~/Desktop/text.csv" # where to write the csv file?
+stephr = 1 # time step in hours for extracting data
+
+# insert your api login details (use yours not mine)
+api_user <- 'OOIAPI-GBZLL30PK7OU67'
+api_token <- 'TEMP-TOKEN-NVSQ4OQM8VH7H7'
+
+# list sites and subset to just Global Irminger (GI) and the surface and two flanking moorings
+sites <- unique(stringr::str_subset(sensors_all$site,"GI"))
+df_sites <- filter(sensors_all, site %in% sites)
+
+sensor_type <- "3-Wavelength Fluorometer"
+depth <- c(0,40)
+sensor_fields <- c("time",
+                   "fluorometric_chlorophyll_a")
+
+sensor_type <- "CTD"
+depth <- c(0,40)
+param_names <- c("time",
+                 "temperature",
+                 "practical_salinity")
+
+df <- filter(df_sites,str_detect(name,sensor_type) & min_depth > depth[1] & max_depth < depth[2])
+params <- get_sensor_meta(df$site[1], df$node[1], df$sensor[1], api_user, api_token)
+inst_id <- unique(as.numeric(stringr::str_extract(filter(params, particleKey %in% param_names)$pdId, "[0-9].*")))
+
+# # NOTE!!! field dependent change --------------------------------------------------
+#
+# # things change here depending on which field you want to extract
+# if (field == "chla") {
+#   # filter df_sites for just the fluorometer sensors - name found in df_sites$name
+#   df <- filter(df_sites,str_detect(name,"^3-Wavelength Fluorometer$"))
+#   # get all the parameters exported in a sensor stream by just looking at the first one
+#   params <- get_sensor_meta(df$site[1], df$node[1], df$sensor[1], api_user, api_token)
+#   # parameters you want to download - see params$pdId column in params (7 = time, 22 = chla)
+#   inst_id = c(7,22)
+# }
+#
+# if (field == "do") {
+#   df <- filter(df_sites,str_detect(name,"^Dissolved Oxygen$"))e
+#   params <- get_sensor_meta(df$site[1], df$node[1], df$sensor[1], api_user, api_token)
+#   inst_id = c(7,14)
+# }
+#
+# if (field == "nit") {
+#   df <- filter(df_sites,str_detect(name,"^Nitrate$"))
+#   params <- get_sensor_meta(df$site[1], df$node[1], df$sensor[1], api_user, api_token)
+#   inst_id = c(7,315)
+# }
+#
+# if (field == "ctd") {
+#   df <- filter(df_sites,str_detect(name,"^CTD$|^CTD \\(30 meters\\)$"))
+#   params <- get_sensor_meta(df$site[2], df$node[2], df$sensor[2], api_user, api_token)
+#   inst_id = c(7,13,193)
+#   inst_id = c(7,13,2927)
+# }
+# continueing on ----------------------------------------------------------
+
+# set the method (recovered_host? telemetered?)
+method <- "recovered_host"
+
+# Get the streams at each site, node, and sensor to make the request to the API
+# Loop through each row of df to find the streams for each and put these into a new data frame
+for (i in 1:nrow(df)) {
+  df2<- filter(df, 1:nrow(df) %in% i)
+  streams <- get_streams(df2$site, df2$node, df2$sensor, method, api_user, api_token)
+  for (j in 1:length(streams)) {
+    dfadd <- mutate(df2, method = method, stream = streams[j])
+    if (i == 1 & j == 1) {
+      dfout <- dfadd
+    } else {
+      dfout <- bind_rows(dfout,dfadd)
+    }
+  }
+}
+
+# Set up a parameter enquirely (this will only request certain parameters)
+query <- paste0("parameters=",paste0(inst_id,collapse = ","))
+
+# loop through the rows of dfout and make a request for each site, node, etc combo
+# save the request details to list (data_request)
+data_request <- vector(mode = "list", length = nrow(dfout))
+for (i in 1:nrow(dfout)) {
+  df2 <- filter(dfout, 1:nrow(dfout) %in% i)
+  data_request[[i]] <- request_data(df2$site,df2$node,df2$sensor,method,df2$stream, api_user, api_token, query = query)
+}
+
+
+
+#####
+# PAUSE - the server needs time to process your request and create your data
+# Give it 5 mins.
+# Have a cup of tea.
+#####
+
+
+
+# Retrieve the data -------------------------------------------------------
+#
+# Loop through list of requests and extract the data for each request
+for (i in 1:length(data_request)) {
+
+  # If there's an error with a request, skip it
+  if("message" %in% names(data_request[[i]])) {
+    message("skipping: ", i)
+    next
+  }
+
+  # get the filenames of all the files generated during the request
+  filenames <- get_filenames(data_request[[i]]) # get all filenames
+
+  # remove some of the filenames that have been causing some trouble (not sure why right now)
+  if (field != "ctd") {
+    filenames <- filenames[!str_detect(filenames, "METBK|CTD")]
+  }
+
+  # loop through filenames, read the data and combine
+  for (file in filenames) {
+    # get the number of data points in the file
+    # This is a real hack - involves reading all the html from a page and then extacting 1 instance of the number of obs
+    file_html <- RCurl::getURL(paste0(file,".html"))
+    n <- as.numeric(stringr::str_extract(file_html,"(?<=time\\[obs \\= )[0-9]*"))-1
+
+    # find the length of the timestep and hence the number of data points to skip to get ~1hr resolution
+    # # this involves downloading a very small data chunk and searching the global attributes for the time step
+    param_add <- "obs[1]"
+    file2 <- file2 <- paste(file,param_add,sep="?")
+    f <- nc_open(file2)
+    timecov <- ncatt_get(f, varid = 0)$time_coverage_resolution
+    dt <- as.numeric(stringr::str_extract(timecov,"(?<=[A-Z]).*(?=[A-Z])"))
+    step <- ceiling(stephr * 3600 / dt)
+
+    # create file request that only selects param_names from 0->n in steps of "step"
+    param_add <- paste(paste0(param_names,"[0:", step, ":", n, "]"), collapse = ",")
+    file2 <- paste(file,param_add,sep="?")
+
+    # read data and combine
+    f <- tidync(file2) %>%
+      hyper_tibble()
+    f <- mutate(f,
+                site = dfout$site[i],
+                node = dfout$node[i],
+                sensor = dfout$sensor[i],
+                method = method,
+                stream = dfout$stream[i],
+                file = file)
+    if (i == 1 & file == filenames[1]) {
+      df <- f
+    } else {
+      df <- bind_rows(df,f)
+    }
+  }
+
+
+
+}
+
+# convert time column to date time format (from seconds since 1900-01-01)
+df <- mutate(df, time = as.POSIXct(time, origin = "1900-01-01"))
+
+if(csv_write) {
+  write_csv(df, csv_file)
+}
